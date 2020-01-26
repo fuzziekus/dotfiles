9d36044adb1cd383b340b699cf4638062b6c0244
fuzziekus <fuzziekus@gmail.com>
2020-01-26 23:30:13 +0900

    fix a bit

--- src/zsh/rc/70_plugin.zsh
+++ src/zsh/rc/70_plugin.zsh
@@ -1,20 +1,4 @@
 # setup zinit
-
-### original
-### Added by Zinit's installer
-#if [[ ! -f $HOME/.config/dotfiles/zsh/.zinit/bin/zinit.zsh ]]; then
-#    print -P "%F{33}▓▒░ %F{220}Installing DHARMA Initiative Plugin Manager (zdharma/zinit)…%f"
-#    command mkdir -p "$HOME/.config/dotfiles/zsh/.zinit" && command chmod g-rwX "$HOME/.config/dotfiles/zsh/.zinit"
-#    command git clone https://github.com/zdharma/zinit "$HOME/.config/dotfiles/zsh/.zinit/bin" && \
-#        print -P "%F{33}▓▒░ %F{34}Installation successful.%f" || \
-#        print -P "%F{160}▓▒░ The clone has failed.%f"
-#fi
-#source "$HOME/.config/dotfiles/zsh/.zinit/bin/zinit.zsh"
-#autoload -Uz _zinit
-#(( ${+_comps} )) && _comps[zinit]=_zinit
-### End of Zinit installer's chunk
-
-
 ## Manual Install
 if [ -z "$ZINIT_HOME" ]; then
     ZINIT_HOME="$XDG_DATA_HOME/zinit"
@@ -32,13 +16,13 @@ source "$ZINIT_HOME/bin/zinit.zsh"
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
-## completion
-zplugin ice wait'!0' lucid as"completion" atload"source $ZHOMEDIR/rc/pluginconfig/zsh-completions_atload.zsh"
-zplugin light 'zsh-users/zsh-completions'
-
 ## highlighting
 zinit ice wait'0' atinit'zpcompinit; zpcdreplay' lucid
 zinit light zdharma/fast-syntax-highlighting
+
+## completion
+zplugin ice wait'!0' lucid as"completion"
+zplugin light 'zsh-users/zsh-completions'
 
 ## auto-pairing
 zinit ice wait'0' lucid
