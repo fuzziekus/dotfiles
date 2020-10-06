7ac8ebea40337dfb48d50c29cf61d92f04205310
fuzziekus <fuzziekus@gmail.com>
2020-10-06 20:49:14 +0900

    fuzzy match -> exact match (history)

--- src/zsh/rc/misc/fzf.zsh
+++ src/zsh/rc/misc/fzf.zsh
@@ -1,7 +1,7 @@
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
 
 function select-history() {
-  BUFFER=$(history -n -r 1 | fzf --no-sort +m --query "$LBUFFER" --prompt="[History] > ")
+  BUFFER=$(history -n -r 1 | fzf -e --no-sort +m --query "$LBUFFER" --prompt="[History] > ")
   zle reset-prompt
   CURSOR=$#BUFFER
 }
